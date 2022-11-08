cad0fe58073bb4126c404c83f805545331f9b58e
MAR <ellen.bjornsdottir@gmail.com>
2022-11-08 11:21:43 +0100

    Audio function click added

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,24 +1,32 @@
 import React from "react";
+import { Howl } from "howler";
 
 export default function Phonetic(props) {
+  let audio = props.phonetic.audio;
+
   function audioSound(event) {
-    event.preventDefault(event);
-
-    return alert("audio Sound");
+    event.preventDefault();
+    var sound = new Howl({
+      src: [audio],
+    });
+    return sound.play();
   }
-
-  return (
-    <div className="Phonetic d-flex flex-direction-row">
-      <div className="audio">
-        <button
-          class="material-symbols-outlined"
-          id="playButton"
-          onClick={audioSound}
-        >
-          play_circle
-        </button>
+  if (audio != null) {
+    return (
+      <div className="Phonetic d-flex flex-direction-row">
+        <div className="audio">
+          <button
+            className="material-symbols-outlined"
+            id="playButton"
+            onClick={audioSound}
+          >
+            play_circle
+          </button>
+        </div>
+        {props.phonetic.text}
       </div>
-      {props.phonetic.text}
-    </div>
-  );
+    );
+  } else {
+    return null;
+  }
 }
